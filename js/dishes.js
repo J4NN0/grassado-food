69cdb99633c724c62c9bdc05d866ec3ce418e867
J4NN0 <giannofederico@gmail.com>
2025-08-22 18:11:27 +0200

    add new dishes

--- js/dishes.js
+++ js/dishes.js
@@ -1,109 +1,118 @@
 // Dishes Data
 const dishes = [
     {
-        id: 1,
+        category: "juices",
+        image: "img/juices/detox.png",
+        name: "Monday Detox",
+        description: "One <b>cucumber</b>, two <b>green apples</b>, two <b>celery</b> ribs, a handful of <b>spinach</b>, and a bit of <b>ginger</b>."
+    },
+    {
+        category: "juices",
+        image: "img/juices/ace.png",
+        name: "Vitamin Boost",
+        description: "ACE juice made with two <b>oranges</b>, two <b>carrots</b>, and one <b>lemon</b>."
+    },
+    {
         category: "brunch",
         image: "img/brunches/omelette.png",
         name: "Omelette",
         description: "French-style omelette with <b>eggs</b> cooked just right. Add optional <b>ham</b> and <b>melty mozzarella</b>. Mix it up however you like - it's always a tasty brunch pick!"
     },
     {
-        id: 2,
         category: "brunch",
         image: "img/brunches/morning-rush.png",
         name: "Morning Rush",
         description: "A hearty blend of <b>scrambled eggs</b> and <b>ham</b>, perfect for a quick and satisfying start to your day. A quick dish to prepare and ready to go in no time."
     },
     {
-        id: 3,
         category: "brunch",
         image: "img/brunches/morning-love.png",
         name: "Morning Love",
         description: "A delightful brunch dish of <b>toasted bread</b> layered with <b>Frischkäss</b> (fresh cheese), topped with <b>scrambled eggs</b> and <b>sliced avocado</b>."
     },
     {
-        id: 4,
         category: "brunch",
         image: "img/brunches/ragnar.png",
         name: "Ragnar",
         description: "Viking-inspired feast of <b>toasted bread</b>, topped with <b>Frischkäss</b> (fresh cheese), <b>scrambled eggs</b>, <b>crispy bacon</b>, <b>rucola</b>, <b>cherry tomatoes</b>, and <b>sliced avocado</b> on the side."
     },
     {
-        id: 5,
         category: "brunch",
         image: "img/brunches/white-lagertha.png",
         name: "White Lagertha",
         description: "A fresh and light brunch featuring <b>toasted bread</b> topped with <b>scrambled eggs</b>, fresh <b>rucola</b>, sliced <b>cherry tomatoes</b>, and creamy <b>burrata cheese</b> to finish."
     },
     {
-        id: 6,
         category: "starters",
         image: "img/starters/pao-de-queijo.png",
         name: "Pão de Queijo",
         description: "A Brazilian cheese bread made with <b>tapioca flour</b>, <b>cheese</b>, and <b>egg</b>."
     },
     {
-        id: 7,
         category: "main",
         image: "img/main-dishes/cannelloni.png",
         name: "Cannelloni",
-        description: "Cannelloni filled with <b>ricotta</b> and <b>spinach</b>, topped with a layer of <b>tomato sauce</b> and <b>besciamella</b>, then baked in the oven."
+        description: "Cannelloni baked in the oven filled with <b>ricotta</b> and <b>spinach</b>, topped with a layer of <b>tomato sauce</b> and <b>besciamella</b>."
     },
     {
-        id: 8,
         category: "main",
         image: "img/main-dishes/pasta-anchovies.png",
         name: "Pasta with Anchovies",
-        description: "<b>Anchovies</b> cooked in <b>olive oil</b> with <b>garlic</b>, then pasta is stirred with cooking water and the anchovy sauce, served with fresh <b>parsley</b>. <b>Breadcrum</b> is toasted and then added on top."
+        description: "<b>Anchovies</b> cooked in <b>olive oil</b> with <b>garlic</b>, then pasta is stirred with cooking water and the anchovy sauce, served with fresh <b>parsley</b>. <b>Breadcrumbs</b> are toasted and then added on top."
     },
     {
-        id: 9,
         category: "main",
         image: "img/main-dishes/pasta-capers.png",
         name: "Pasta with Capers",
         description: "A creamy sauce made from <b>capers</b> and <b>garlic</b> is prepared, then lightly fried in a pan with oil. <b>Tomato sauce</b> is added and simmered, before being tossed with pasta until well combined."
     },
     {
-        id: 10,
         category: "main",
         image: "img/main-dishes/pasta-mushrooms-bacon.png",
         name: "Pasta with Mushrooms and Bacon",
-        description: "This dish is prepared by first cooking the <b>bacon</b> until crispy. Using the remaining bacon fat, <b>mushrooms</b> are sautéed with a touch of <b>black pepper</b>. Everything is then stirred together in the pan, incorporating a bit of the <b>pasta's boiling water</b> to create a rich and savory sauce."
+        description: "The <b>bacon</b> is cooked until crispy, <b>mushrooms</b> are sautéed in the remaining bacon fat with a touch of <b>black pepper</b>. Everything is then stirred together in the pan, incorporating a bit of the <b>pasta's boiling water</b> to create a rich and savory sauce."
     },
     {
-        id: 11,
+        category: "main",
+        image: "img/main-dishes/pasta-broccoli.png",
+        name: "Pasta with Broccoli",
+        description: "The <b>broccoli</b> are first boiled until soft, then sautéed in a pan with <b>garlic</b>. The <b>pasta</b> is cooked in the same water used for the broccoli, then combined and fried together with the broccoli and garlic at the end for extra flavor."
+    },
+    {
         category: "main",
         image: "img/main-dishes/rice-bacon-peas.png",
         name: "Risotto with Bacon and Peas",
         description: "Rice toasted with butter, then deglazed with white wine. The rice is then slowly cooked using <b>vegetable stock</b>, then complemented by crispy <b>bacon</b> and <b>green peas</b>."
     },
     {
-        id: 12,
         category: "main",
         image: "img/main-dishes/rice-shrimps-lemon.png",
         name: "Risotto with Shrimps and Lemon",
         description: "Rice toasted with onions, then deglazed with white wine. As the rice slowly absorbs flavorful <b>vegetable stock</b>, <b>shrimps</b> and bright <b>lemon zest</b> are folded in."
     },
     {
-        id: 13,
         category: "main",
         image: "img/main-dishes/rice-tomato-burrata.png",
         name: "Risotto with Tomato Sauce and Burrata",
         description: "Rice toasted with onions, then deglazed with white wine. The rice is slowly cooked with <b>tomato sauce</b> and <b>vegetable stock</b>, then topped with cold fresh <b>burrata</b> and a touch of <b>pesto</b>."
     },
     {
-        id: 14,
         category: "main",
         image: "img/main-dishes/rice-beef.png",
         name: "Rice with Beef",
         description: "Tender <b>beef strips</b> cooked in a rich sauce made with <b>tomato paste</b>, <b>mustard</b>, <b>soy sauce</b>, and <b>cream</b>, with sautéed <b>mushrooms</b> added at the end, served alongside white <b>rice</b>."
     },
     {
-        id: 15,
         category: "second",
         image: "img/second-dishes/ossobuco.png",
         name: "Ossobuco with Smashed Potatoes",
         description: "<b>Ossobuco</b> is first fried until golden on each side, then slowly cooked with <b>onions</b>, <b>carrots</b>, <b>celery</b> and <b>beef stock</b> until tender. Served with <b>smashed potatoes</b>."
+    },
+    {
+        category: "dessert",
+        image: "img/desserts/banana-bread.png",
+        name: "Banana Bread",
+        description: "Cake made with <b>bananas</b>, <b>eggs</b>, <b>flour</b>, <b>cinnamon</b>, <b>milk</b>, <b>nuts</b>, <b>vanilla</b>, and <b>butter</b>."
     }
 ];
 
